e51aeb633bf77dbfef7b9801ab65380d752552ec
fchennouf <fouad.chennouf@gmail.com>
2021-05-24 10:14:21 +0200

    Update OutsideClick.js

--- src/lib/OutsideClick.js
+++ src/lib/OutsideClick.js
@@ -24,7 +24,8 @@ export default class OutsideClick extends PureComponent {
 
     componentDidMount() {
         const { onOutsideClick } = this.props;
-
+        // On razzle part when the component mount is trigger onOutsideClick for obscure reason
+        // we add a sh*** code to avoid this. This is very dirthy
         if (onOutsideClick) {
             setTimeout(() => {
                 document.addEventListener(this.EVENT, this.onOutsideClick);
